de2b50e61dab935992d24dc18a13c8cf562f842d
Dayvisson Tenorio <dayvisson.tenorio@gmail.com>
2024-05-11 20:47:22 -0300

    Corrigido o ajuste da formatação do texto ao ser importado para o arquivo perfil.xlsl

--- main.py
+++ main.py
@@ -31,27 +31,27 @@ def editar_info(perfil):
 
     edit_matricula = tk.Entry(window_editar)
     edit_matricula.insert(0, perfil.get('Matricula', ''))
-    edit_matricula.grid(row=1, column=1)
+    edit_matricula.grid(row=1, column=2)
     tk.Label(window_editar, text='Matrícula:').grid(row=1, column=0)
 
     edit_celular = tk.Entry(window_editar)
     edit_celular.insert(0, perfil.get('Celular', ''))
-    edit_celular.grid(row=2, column=1)
+    edit_celular.grid(row=2, column=3)
     tk.Label(window_editar, text='Número de Celular:').grid(row=2, column=0)
 
     edit_cargo = tk.Entry(window_editar)
     edit_cargo.insert(0, perfil.get('Cargo', ''))
-    edit_cargo.grid(row=3, column=1)
+    edit_cargo.grid(row=3, column=4)
     tk.Label(window_editar, text='Cargo:').grid(row=3, column=0)
 
     edit_idade = tk.Entry(window_editar)
     edit_idade.insert(0, perfil.get('Idade', ''))
-    edit_idade.grid(row=4, column=1)
+    edit_idade.grid(row=4, column=5)
     tk.Label(window_editar, text='Idade:').grid(row=4, column=0)
 
     edit_ministerio = tk.Entry(window_editar)
     edit_ministerio.insert(0, perfil.get('Ministerio', ''))
-    edit_ministerio.grid(row=5, column=1)
+    edit_ministerio.grid(row=5, column=6)
     tk.Label(window_editar, text='Ministério:').grid(row=5, column=0)
 
     btn_salvar = tk.Button(window_editar, text='Salvar', command=salvar)
