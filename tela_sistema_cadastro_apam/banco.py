23d3e544162e8e9ca738363d10d94b2cc7d68597
Veras-D <dveras2310@gmail.com>
2024-05-20 16:01:43 -0300

    working on db

--- tela_sistema_cadastro_apam/banco.py
+++ tela_sistema_cadastro_apam/banco.py
@@ -1,13 +1,16 @@
 import sqlite3
 from tkinter import messagebox
 
+
 class RegistrationSystem:
     def __init__(self):
-        self.conn = sqlite3.connect('bancoapam.db')
+        self.conn = sqlite3.connect('bancoAPAM.db')
         self.c = self.conn.cursor()
-        self.create_table()
+        self.create_tables()
 
-    def create_table(self):
+
+# Corrigir tipagem de dados
+    def create_tables(self):
         self.c.execute('''CREATE TABLE IF NOT EXISTS bancoapam
                             (id INTEGER PRIMARY KEY AUTOINCREMENT,
                             name TEXT NOT NULL,
@@ -16,24 +19,104 @@ class RegistrationSystem:
                             email TEXT NOT NULL,
                             telefone TEXT NOT NULL,
                             sexo TEXT NOT NULL,
-                            data TEXT NOT NULL,
+                            data DATE NOT NULL,
                             endereco TEXT NOT NULL,
                             temperamento TEXT NOT NULL,
                             imagem TEXT NOT NULL)''')
         self.conn.commit()
 
+
+        self.c.execute('''CREATE TABLE IF NOT EXISTS animal
+                            (id_animal INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name_animal TEXT NOT NULL,
+                            data_cadastro DATE NOT NULL,
+                            especie TEXT NOT NULL,
+                            genero TEXT NOT NULL,
+                            idade_anos INTEGER,
+                            idade_meses INTEGER,
+                            porte TEXT NOT NULL,
+                            pelagem TEXT NOT NULL,
+                            raca TEXT NOT NULL)''')
+        self.conn.commit()
+
+
+        self.c.execute('''CREATE TABLE IF NOT EXISTS resgate
+                            (id_resgate INTEGER PRIMARY KEY AUTOINCREMENT,
+                            id_animal INTEGER,
+                            local_resgate TEXT NOT NULL,
+                            atendimento TEXT NOT NULL,
+                            necessario_intervencao_cirurgica TEXT NOT NULL,
+                            destinacao_do_protegido TEXT NOT NULL,
+                            historico_anamnese TEXT NOT NULL,
+                            diagnostico_estado_saude TEXT NOT NULL,
+                            tratamento_intervencao_e_medicacao TEXT NOT NULL,
+                            data_resgate DATE NOT NULL,
+                            castrado TEXT NOT NULL,
+                            data_castracao DATE NOT NULL,
+                            obito TEXT NOT NULL,
+                            status_atual TEXT NOT NULL,
+                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+        self.conn.commit()
+
+
+        self.c.execute('''CREATE TABLE IF NOT EXISTS observacoes
+                            (id_obs INTEGER PRIMARY KEY AUTOINCREMENT,
+                            id_animal INTEGER,
+                            data DATE NOT NULL,
+                            observacao TEXT NOT NULL,
+                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+        self.conn.commit()
+
+
+        self.c.execute('''CREATE TABLE IF NOT EXISTS vacinas
+                            (id_vacinas INTEGER PRIMARY KEY AUTOINCREMENT,
+                            id_animal INTEGER,
+                            data_vacina DATE NOT NULL,
+                            peso REAL NOT NULL,
+                            medicamento TEXT NOT NULL,
+                            dose TEXT NOT NULL,
+                            data_proxima_dose DATE NOT NULL,
+                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+        self.conn.commit()
+
+
+        self.c.execute('''CREATE TABLE IF NOT EXISTS adocao
+                            (id_adocao INTEGER PRIMARY KEY AUTOINCREMENT,
+                            id_animal INTEGER,
+                            name_adotante TEXT NOT NULL,
+                            rg TEXT NOT NULL,
+                            cpf TEXT,
+                            rua TEXT NOT NULL,
+                            numero_rua TEXT NOT NULL,
+                            complemento TEXT NOT NULL,
+                            cep TEXT NOT NULL,
+                            bairro TEXT NOT NULL,
+                            cidade TEXT NOT NULL,
+                            uf TEXT NOT NULL,
+                            profissao TEXT NOT NULL,
+                            email TEXT NOT NULL,
+                            telefone_fixo TEXT NOT NULL,
+                            celular TEXT NOT NULL,
+                            referencia_rua TEXT NOT NULL,
+                            data_adocao DATE NOT NULL,
+                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+        self.conn.commit()
+
+
     def register_apam(self, apam):
-        self.c.execute("INSERT INTO bancoapam(name, cpf, data, email, endereco, matricula, telefone, sexo, temperamento, imagem) VALUES (?,?,?,?,?,?,?,?,?,?)", 
+        self.c.execute("INSERT INTO bancoapam(name, cpf, data, email, endereco, matricula, telefone, sexo, temperamento, imagem) VALUES (?,?,?,?,?,?,?,?,?,?)",
                         (apam))
         self.conn.commit()
         # Mostrando mesnsagem de sucesso
         messagebox.showinfo('Sucesso', 'Registrado com Sucesso!')
         print("Registrado com Sucesso!")
 
+
     def view_all_bancoapam(self):
         self.c.execute("SELECT * FROM bancoapam")
         data = self.c.fetchall()
         return data
+
 
     def search_apam(self, id):
         self.c.execute("SELECT * FROM bancoapam WHERE id=?", (id,))
@@ -42,6 +125,7 @@ class RegistrationSystem:
             return data
         else:
             messagebox.showerror('Erro', f'Nenhum ID {id} encontrado!')
+
 
     def update_apam(self, valor_atualizado):
         data = valor_atualizado
@@ -53,6 +137,7 @@ class RegistrationSystem:
         else:
             messagebox.showerror('Erro', f"Nenhum ID {valor_atualizado[10]} encontrado!")
             print(f"Nenhum ID '{valor_atualizado[10]}' encontrado!")
+
 
     def delete_apam(self, id):
         self.c.execute("SELECT * FROM bancoapam WHERE id=?", (id,))
