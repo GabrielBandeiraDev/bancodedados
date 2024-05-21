d7ae56d7fa426c0a467ca251dfd1e00047e6f31d
Veras-D <dveras2310@gmail.com>
2024-05-20 22:25:42 -0300

    "Updated .gitignore to ignore test.py, added import pandas to banco.py, modified RegistrationSystem class to include get_columns method, updated create_tables and register_apam methods, added export_to_excel method, and made minor changes to main.py"

--- tela_sistema_cadastro_apam/banco.py
+++ tela_sistema_cadastro_apam/banco.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 from tkinter import messagebox
 
 
@@ -7,6 +8,16 @@ class RegistrationSystem:
         self.conn = sqlite3.connect('bancoAPAM.db')
         self.c = self.conn.cursor()
         self.create_tables()
+
+
+    def get_columns(self, table: str='bancoapam'):
+        self.c.execute(f"PRAGMA table_info({table})")
+        db = self.c.fetchall()
+        colunas = []
+        for coluna_db in db:
+            colunas.append(coluna_db[1])
+        self.conn.commit()
+        return colunas
 
 
 # Corrigir tipagem de dados
@@ -55,7 +66,7 @@ class RegistrationSystem:
                             data_castracao DATE NOT NULL,
                             obito TEXT NOT NULL,
                             status_atual TEXT NOT NULL,
-                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+                            FOREIGN KEY (id_animal) REFERENCES animal(id_animal))''')
         self.conn.commit()
 
 
@@ -64,7 +75,7 @@ class RegistrationSystem:
                             id_animal INTEGER,
                             data DATE NOT NULL,
                             observacao TEXT NOT NULL,
-                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+                            FOREIGN KEY (id_animal) REFERENCES animal(id_animal))''')
         self.conn.commit()
 
 
@@ -76,7 +87,7 @@ class RegistrationSystem:
                             medicamento TEXT NOT NULL,
                             dose TEXT NOT NULL,
                             data_proxima_dose DATE NOT NULL,
-                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+                            FOREIGN KEY (id_animal) REFERENCES animal(id_animal))''')
         self.conn.commit()
 
 
@@ -99,27 +110,31 @@ class RegistrationSystem:
                             celular TEXT NOT NULL,
                             referencia_rua TEXT NOT NULL,
                             data_adocao DATE NOT NULL,
-                            FOREIGN KEY (id_animal) REFERENCES animal(id))''')
+                            FOREIGN KEY (id_animal) REFERENCES animal(id_animal))''')
         self.conn.commit()
 
 
-    def register_apam(self, apam):
-        self.c.execute("INSERT INTO bancoapam(name, cpf, data, email, endereco, matricula, telefone, sexo, temperamento, imagem) VALUES (?,?,?,?,?,?,?,?,?,?)",
-                        (apam))
+    def register_apam(self, values_table: list, table: str='bancoapam'):
+        colunas = self.get_columns(table=table)
+        inter = ['?'] * len(colunas)  # Lista de valores para serem inseridos na consulta
+        query = f"INSERT INTO {table}({', '.join(colunas)}) VALUES ({','.join(inter)})"
+        self.c.execute(query, tuple(values_table))
         self.conn.commit()
+
+
         # Mostrando mesnsagem de sucesso
         messagebox.showinfo('Sucesso', 'Registrado com Sucesso!')
         print("Registrado com Sucesso!")
 
 
-    def view_all_bancoapam(self):
-        self.c.execute("SELECT * FROM bancoapam")
+    def view_all_bancoapam(self, table: str='bancoapam'):
+        self.c.execute(f"SELECT * FROM {table}")
         data = self.c.fetchall()
         return data
 
 
-    def search_apam(self, id):
-        self.c.execute("SELECT * FROM bancoapam WHERE id=?", (id,))
+    def search_apam(self, id, table: str='bancoapam'):
+        self.c.execute(f"SELECT * FROM {table} WHERE id=?", (id,))
         data = self.c.fetchone()
         if data:
             return data
@@ -127,10 +142,11 @@ class RegistrationSystem:
             messagebox.showerror('Erro', f'Nenhum ID {id} encontrado!')
 
 
-    def update_apam(self, valor_atualizado):
+    def update_apam(self, valor_atualizado, table: str='bancoapam'):
         data = valor_atualizado
+        colunas = self.get_columns(table=table)
         if data:
-            query = "UPDATE bancoapam SET name=?, cpf=?, data=?, email=?, endereco=?, matricula=?, telefone=?, sexo=?, temperamento=?, imagem=? WHERE id=?"
+            query = f"UPDATE {table} SET name=?, cpf=?, data=?, email=?, endereco=?, matricula=?, telefone=?, sexo=?, temperamento=?, imagem=? WHERE id=?"  # Corrigir parâmetros
             self.c.execute(query,valor_atualizado)
             self.conn.commit()
             messagebox.showinfo('Sucesso', f'Informação do ID {valor_atualizado[10]} foi atualizado!')
@@ -139,17 +155,26 @@ class RegistrationSystem:
             print(f"Nenhum ID '{valor_atualizado[10]}' encontrado!")
 
 
-    def delete_apam(self, id):
-        self.c.execute("SELECT * FROM bancoapam WHERE id=?", (id,))
+    def delete_apam(self, id, table: str='bancoapam'):
+        self.c.execute(f"SELECT * FROM {table} WHERE id=?", (id,))
         data = self.c.fetchone()
         if data:
-            self.c.execute("DELETE FROM bancoapam WHERE id=?", (id,))
+            self.c.execute(f"DELETE FROM {table} WHERE id=?", (id,))
             self.conn.commit()
             messagebox.showinfo('Sucesso', f"Informação do ID {id} foi deletado!")
             print(f"Informação do ID '{id}' foi deletado!")
         else:
             messagebox.showerror('Erro', f"Nenhum ID {id} encontrado!")
             print(f"Nenhum ID '{id}' encontrado!")
+
+
+    def export_to_excel(self, table: str):
+        self.c.execute(f"SELECT *, oid FROM {table}")
+        to_excel = self.c.fetchall()
+        colunas = self.get_columns(table=table)
+        to_excel = pd.DataFrame(to_excel, columns=colunas)
+        to_excel.to_excel(f'{table}.xlsx')
+        self.conn.commit()
 
 
 # create a registration system instance
