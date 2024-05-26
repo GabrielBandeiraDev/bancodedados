cd5e0786ba04e64483420e5a7af1732f143f3f0d
Veras-D <dveras2310@gmail.com>
2024-05-26 05:34:30 -0300

    finish formatting of fields

--- tela_sistema_cadastro_apam/flet_main.py
+++ tela_sistema_cadastro_apam/flet_main.py
@@ -6,8 +6,11 @@ from util import *
 def main(page: ft.Page):
     def cadastrar(e):
         if not Validacao.validarNome(nome.value):
-            nome.error_text = "Preencha o nome"
-        
+            nome.error_text = "Preencha o Campo Nome Adequadamente"
+        if not Validacao.validarCPF(cpf.value):
+            cpf.error_text = "Preencha o Campo CPF Adequadamente"
+        if not Validacao.validarRG(rg.value):
+            rg.error_text = "Preencha o Campo RG Adequadamente"
         page.update()
 
     
@@ -30,20 +33,204 @@ def main(page: ft.Page):
         )
     cpf = ft.TextField(
         label='CPF',
-        focused_border_color=ft.colors.BLACK, 
+        focused_border_color=ft.colors.BLACK,
         value='',
         width=300, 
-        text_align=ft.TextAlign.CENTER, 
-        color=ft.colors.WHITE, 
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
         border_radius=10
         )
     rg = ft.TextField(
         label='RG',
         focused_border_color=ft.colors.BLACK,
-        value='', width=300,
+        value='',
+        width=300,
         text_align=ft.TextAlign.CENTER,
         color=ft.colors.WHITE,
         border_radius=10
+        )
+    email = ft.TextField(
+        label='Email',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    estado_civil = ft.Dropdown(
+        label='Estado Civil',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        options=[
+            ft.dropdown.Option('Solteiro(a)'),
+            ft.dropdown.Option('Casado(a)'),
+            ft.dropdown.Option('Divorciado(a)'),
+            ft.dropdown.Option('Viúvo(a)'),
+            ft.dropdown.Option('Outro')
+        ])
+    celular = ft.TextField(
+        label='Telefone Celular',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    sexo = ft.Dropdown(
+        label='Sexo',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        options=[
+            ft.dropdown.Option('M'),
+            ft.dropdown.Option('F'),
+            ft.dropdown.Option('Prefiro Não Responder')
+        ])
+    # Pesq como trabalhar com datas no flet
+    data = ft.TextField(
+        label='Data',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    data_nasc = ft.TextField(
+        label='Data de Nascimento',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    naturalidade = ft.Dropdown(
+        label='Naturalidade',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        options=[
+        ft.dropdown.Option('AC'),
+        ft.dropdown.Option('AL'),
+        ft.dropdown.Option('AM'),
+        ft.dropdown.Option('AP'),
+        ft.dropdown.Option('BA'),
+        ft.dropdown.Option('CE'),
+        ft.dropdown.Option('DF'),
+        ft.dropdown.Option('ES'),
+        ft.dropdown.Option('GO'),
+        ft.dropdown.Option('MA'),
+        ft.dropdown.Option('MG'),
+        ft.dropdown.Option('MS'),
+        ft.dropdown.Option('MT'),
+        ft.dropdown.Option('PA'),
+        ft.dropdown.Option('PB'),
+        ft.dropdown.Option('PE'),
+        ft.dropdown.Option('PI'),
+        ft.dropdown.Option('PR'),
+        ft.dropdown.Option('RJ'),
+        ft.dropdown.Option('RN'),
+        ft.dropdown.Option('RO'),
+        ft.dropdown.Option('RR'),
+        ft.dropdown.Option('RS'),
+        ft.dropdown.Option('SC'),
+        ft.dropdown.Option('SE'),
+        ft.dropdown.Option('SP'),
+        ft.dropdown.Option('TO')
+    ])
+    # Botão para escolher no mapa, google maps API
+    endereco = ft.TextField(
+        label='Endereço',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10)
+    valor_colaborar = ft.TextField(
+        label='Com Qual Valor Pode Colaborar',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    empresa_trabalha = ft.TextField(
+        label='Nome da Empresa em que Trabalha',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    endereco_empresa_trabalha = ft.TextField(
+        label='Endereço Empresa em que Trabalha',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    profisao = ft.TextField(
+        label='Profissão',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=300,
+        text_align=ft.TextAlign.CENTER,
+        color=ft.colors.WHITE,
+        border_radius=10
+        )
+    ajudar_APAM = ft.TextField(
+        label='Como Pode Ajuar a APAM?',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=920,
+        text_align=ft.TextAlign.LEFT,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        multiline=True,
+        max_lines=5,
+        min_lines=1,
+        max_length=500
+        )
+    outras_ajudas = ft.TextField(
+        label='Outras Formas de Ajudar a APAM',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=920,
+        text_align=ft.TextAlign.LEFT,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        multiline=True,
+        max_lines=5,
+        min_lines=1,
+        max_length=500
+        )
+    expectativas = ft.TextField(
+        label='Quais São Suas Expectativas com Relação ao Trabalho Voluntario',
+        focused_border_color=ft.colors.BLACK,
+        value='',
+        width=920,
+        text_align=ft.TextAlign.LEFT,
+        color=ft.colors.WHITE,
+        border_radius=10,
+        multiline=True,
+        max_lines=5,
+        min_lines=1,
+        max_length=500
         )
 
     page.add(
@@ -58,45 +245,39 @@ def main(page: ft.Page):
                 cpf,
                 rg],
                 alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([
-                ft.TextField(label='Email', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Estado Civil', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='telefone Celular', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10)
-            ], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([
-                ft.TextField(label='Sexo', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Data', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Data de Nascimento', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10)
-            ], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([
-                ft.TextField(label='Naturalidade', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Endereço', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Com Qual Valor Pode Colaborar', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10)
-            ], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([
-                ft.TextField(label='Nome da Empresa em que Trabalha', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Endereço Empresa em que Trabalha', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10),
-                ft.TextField(label='Profissão', focused_border_color=ft.colors.BLACK, value='', width=300, text_align=ft.TextAlign.CENTER, color=ft.colors.WHITE, border_radius=10)
-            ], alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row(
+                [email,
+                estado_civil,
+                celular], 
+                alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row(
+                [sexo,
+                 data,
+                 data_nasc],
+                alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row(
+                [naturalidade,
+                 endereco,
+                 valor_colaborar], 
+                alignment=ft.MainAxisAlignment.CENTER),
+            ft.Row(
+                [empresa_trabalha,
+                 endereco_empresa_trabalha,
+                 profisao], 
+                alignment=ft.MainAxisAlignment.CENTER),
             
             ft.Container(height=30),
             
             ft.Column([
-                ft.Row([
-                    ft.TextField(label='Como Pode Ajuar a APAM?', focused_border_color=ft.colors.BLACK, value='', width=920,
-                             text_align=ft.TextAlign.LEFT, color=ft.colors.WHITE, border_radius=10, multiline=True,
-                             max_lines=5, min_lines=1, max_length=500)
-                    ], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Row([
-                    ft.TextField(label='Outras Formas de Ajudar a APAM', focused_border_color=ft.colors.BLACK, value='', width=920,
-                             text_align=ft.TextAlign.LEFT, color=ft.colors.WHITE, border_radius=10, multiline=True,
-                             max_lines=5, min_lines=1, max_length=500)
-                    ], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Row([
-                    ft.TextField(label='Quais São Suas Expectativas com Relação ao Trabalho Voluntario', focused_border_color=ft.colors.BLACK, value='', width=920,
-                             text_align=ft.TextAlign.LEFT, color=ft.colors.WHITE, border_radius=10, multiline=True,
-                             max_lines=5, min_lines=1, max_length=500),
-                    ], alignment=ft.MainAxisAlignment.CENTER)
+                ft.Row(
+                    [ajudar_APAM],
+                    alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row(
+                    [outras_ajudas],
+                    alignment=ft.MainAxisAlignment.CENTER),
+                ft.Row(
+                    [expectativas],
+                    alignment=ft.MainAxisAlignment.CENTER)
             ])
         ]),
         
