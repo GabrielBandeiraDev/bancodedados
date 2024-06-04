465265f793d06f4ed836e81c8ef7d24efec5c65b
Veras-D <dveras2310@gmail.com>
2024-06-03 21:26:01 -0300

    commit

--- tela_sistema_cadastro_apam/main.py
+++ tela_sistema_cadastro_apam/main.py
@@ -144,8 +144,6 @@ def validar(lista: list):
         messagebox.showerror('Erro', 'Valor a Colaborar Invalido')
         return True
 
-
-
 # ------------- funcoes para CRUD ---------------
 
 def adicionar():    
