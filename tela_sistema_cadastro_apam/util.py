12b567ce171e92009ea111dc350ebf1f68a5a143
Veras-D <dveras2310@gmail.com>
2024-06-05 19:45:40 -0300

    "Updated Python version to 3.10.12 in GitHub workflow and commented out validarUF method in util.py"

--- tela_sistema_cadastro_apam/util.py
+++ tela_sistema_cadastro_apam/util.py
@@ -69,12 +69,12 @@ class Validacao:
             return False
         return True
 
-    @staticmethod
-    def validarUF(uf):
-        uf = Validacao.tratarUF(uf).upper()
-        estados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE",
-                   "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
-        return uf in estados
+    # @staticmethod
+    # def validarUF(uf):
+    #     uf = Validacao.tratarUF(uf).upper()
+    #     estados = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE",
+    #                "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
+    #     return uf in estados
 
     @staticmethod
     def validarIdade(idade):
